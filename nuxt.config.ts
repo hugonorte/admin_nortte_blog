2f12fae5afc9fd0d06ca0ebffa3bb8266192b162
Hugo Norte <hugonorte@gmail.com>
2026-07-26 13:28:45 -0300

    refactor: remove hardcoded fallback values for public runtime configuration variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,9 +19,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api',
-      siteName: process.env.NUXT_PUBLIC_SITE_NAME || 'Nortte Blog',
-      publicImagesFolder: process.env.NUXT_PUBLIC_IMAGES_FOLDER || 'http://localhost:8080/public',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+      siteName: process.env.NUXT_PUBLIC_SITE_NAME,
+      publicImagesFolder: process.env.NUXT_PUBLIC_IMAGES_FOLDER,
     }
   },
   vite: {
