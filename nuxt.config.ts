1f1ec20d8c7e80972de40674edbcbcd901a445c0
Hugo Norte <hugonorte@gmail.com>
2026-07-26 14:58:55 -0300

    chore: update nuxt compatibilityDate to 2025-07-26

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-07-26',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon','@nuxt/ui'],
   app: {
